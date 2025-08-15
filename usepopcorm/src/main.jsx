884f53f104ff545c3df87ef831724b1e7dfb48cd
Abdulai059 <abdulaiosman8080@gmail.com>
2025-08-15 17:18:03 +0000

    update

--- usepopcorm/src/main.jsx
+++ usepopcorm/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+import SterRating from "./SterRating";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <SterRating maxRating={5} />
+    <SterRating maxRating={10} />
+    <SterRating />
+  </StrictMode>
+);
