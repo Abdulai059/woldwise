9de01c8225ae3e21d76dc2ad3a3e767fa6e2b383
Abdulai059 <abdulaiosman8080@gmail.com>
2025-09-02 08:14:19 +0000

    initial commit

--- worldwise/src/components/AppNav.jsx
+++ worldwise/src/components/AppNav.jsx
@@ -3,7 +3,7 @@ import styles from "./AppNav.module.css";
 
 function AppNav() {
   return (
-    <nav className={styles.nav}>
+    <nav className={styles.nav}> 
       <ul>
         <li>
           <NavLink to="cities">Cities</NavLink>
