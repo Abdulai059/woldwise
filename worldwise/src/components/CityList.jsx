df79c887c42076204639e07ce0e5e1454b4009a2
Abdulai059 <abdulaiosman8080@gmail.com>
2025-09-03 21:37:29 +0000

    Update logic

--- worldwise/src/components/CityList.jsx
+++ worldwise/src/components/CityList.jsx
@@ -2,14 +2,14 @@
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import CityItem from "./CityItem";
-// import Message from "./Message";
+import Message from "./Message";
 
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
-  // if (!cities.length)
-  //   return (
-  //     <Message message="Add your first city by clicking on the a city on the map" />
-  //   );
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on the a city on the map" />
+    );
 
   return (
     <ul className={styles.cityList}>
